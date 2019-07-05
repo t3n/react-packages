12482d99085a05a17a1e3fe74956d9062fff486a
Philip Plagge <sayhi@itscursedphil.com>
2019-07-05 10:08:19 +0200

    Add knobs to InputGroup story

--- packages/t3n-storybook/src/stories/form/inputGroup/inputGroup.stories.tsx
+++ packages/t3n-storybook/src/stories/form/inputGroup/inputGroup.stories.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
 
 import { storiesOf } from '@storybook/react';
 import { InputGroup } from '@t3n/components';
 
 import StoryContainer from '../../../components/StoryContainer';
 
-storiesOf('Form|InputGroup', module).add('Simple InputGroup', () => (
-  <StoryContainer>
-    <InputGroup
-      type="password"
-      label="Password"
-      placeholder="Passwort eingeben"
-      onChange={e => {
-        console.log(e.currentTarget.value);
-      }}
-    />
-  </StoryContainer>
-));
+storiesOf('Form|InputGroup', module)
+  .addDecorator(withKnobs)
+  .add('Simple InputGroup', () => (
+    <StoryContainer>
+      <InputGroup
+        type={select(
+          'type',
+          {
+            text: 'text',
+            email: 'email',
+            password: 'password'
+          },
+          'text'
+        )}
+        label={text('label', 'Label')}
+        placeholder={text('placeholder', 'Text eingeben...')}
+        onChange={e => {
+          console.log(e.currentTarget.value);
+        }}
+      />
+    </StoryContainer>
+  ));
