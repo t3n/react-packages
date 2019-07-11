2a840a4e04df6b7ffbf94ea60e99d415fc07c62a
Philip Plagge <sayhi@itscursedphil.com>
2019-07-11 15:25:12 +0200

    Disable awesome-typescript-loader caches

--- packages/t3n-storybook/.storybook/webpack.config.js
+++ packages/t3n-storybook/.storybook/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = ({ config }) => {
         loader: require.resolve('awesome-typescript-loader'),
         options: {
           forceIsolatedModules: true,
-          useCache: true,
+          // useCache: true,
           useBabel: true,
           babelCore: '@babel/core',
           reportFiles: ['src/**/*.{ts,tsx}']
