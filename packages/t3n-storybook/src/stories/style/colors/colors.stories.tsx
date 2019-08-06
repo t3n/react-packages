bb25ea50eef917d2e53cdd3a6028bb0cb197e125
Johannes Steu <hello@johannessteu.de>
2019-08-06 11:55:11 +0200

    Show all colors in color story

--- packages/t3n-storybook/src/stories/style/colors/colors.stories.tsx
+++ packages/t3n-storybook/src/stories/style/colors/colors.stories.tsx
@@ -9,11 +9,48 @@ import {
   Heading,
   Text,
   Grid,
-  GridItem
+  GridItem,
+  Section
 } from '@t3n/components';
+import { RgbColor, RgbaColor } from 'polished/lib/types/color';
 import StoryContainer from '../../../components/StoryContainer';
 import intro from './introduction.md';
 import colorsMd from './colors.md';
+
+interface ColorCardProps {
+  name: string;
+  value: string;
+  rgb: RgbColor | RgbaColor;
+}
+
+const ColorCard = ({ name, value, rgb }: ColorCardProps) => (
+  <Card>
+    <CardHeader ratio={3 / 2} bg={value} />
+    <Heading as="h3" my={0}>
+      {name}
+    </Heading>
+    <Text mb={0}>
+      <strong>{value}</strong>
+      <br />
+      {Object.entries(rgb).map(([colorName, colorValue]) => {
+        return (
+          <span
+            key={String(colorName)}
+            style={{
+              display: 'inline-flex',
+              width: '100%',
+              justifyContent: 'space-between'
+            }}
+          >
+            <strong style={{ flex: 1 }}>{colorName[0].toUpperCase()}:</strong>{' '}
+            <span style={{ flex: 4 }}>{colorValue}</span>
+            <br />
+          </span>
+        );
+      })}
+    </Text>
+  </Card>
+);
 
 storiesOf('Visual Identity|Colors', module)
   .addDecorator(story => <StoryContainer>{story()}</StoryContainer>)
@@ -23,53 +60,38 @@ storiesOf('Visual Identity|Colors', module)
     }
   })
   .add(
-    'Brand Colors',
+    'Online Farben',
     () => (
       <ThemeConsumer>
         {theme => {
-          return (
-            <Grid my={-1}>
-              {Object.keys(theme.colors.brand).map(name => {
-                const value = theme.colors.brand[name];
-                const rgb = parseToRgb(theme.colors.brand[name]);
+          const categories = Object.keys(theme.colors);
 
-                return (
-                  <GridItem key={name} width={[1, 1 / 3, 1 / 4]} my={1}>
-                    <Card>
-                      <CardHeader ratio={3 / 2} bg={value} />
-                      <Heading as="h3" my={0}>
-                        {name}
-                      </Heading>
-                      <Text mb={0}>
-                        <strong>{value}</strong>
-                        <br />
-                        {Object.entries(rgb).map(([colorName, colorValue]) => {
-                          console.log(colorName, colorValue);
+          console.log(categories);
+          return categories.map(name => (
+            <Section>
+              <Heading as="h2">
+                {name.charAt(0).toUpperCase() + name.slice(1)}
+              </Heading>
 
-                          return (
-                            <span
-                              key={String(colorName)}
-                              style={{
-                                display: 'inline-flex',
-                                width: '100%',
-                                justifyContent: 'space-between'
-                              }}
-                            >
-                              <strong style={{ flex: 1 }}>
-                                {colorName[0].toUpperCase()}:
-                              </strong>{' '}
-                              <span style={{ flex: 4 }}>{colorValue}</span>
-                              <br />
-                            </span>
-                          );
-                        })}
-                      </Text>
-                    </Card>
-                  </GridItem>
-                );
-              })}
-            </Grid>
-          );
+              <Grid my={-1}>
+                {Object.keys(theme.colors[name]).map(colorName => {
+                  const value = theme.colors[name][colorName];
+                  const rgb = parseToRgb(theme.colors[name][colorName]);
+
+                  return (
+                    <GridItem key={name} width={[1, 1 / 4, 1 / 5]} my={1}>
+                      <ColorCard
+                        key={name}
+                        name={colorName}
+                        rgb={rgb}
+                        value={value}
+                      />
+                    </GridItem>
+                  );
+                })}
+              </Grid>
+            </Section>
+          ));
         }}
       </ThemeConsumer>
     ),
