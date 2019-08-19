eef9b6a016cb0bba51c11c44c57b7daa9037568c
Philip Plagge <sayhi@itscursedphil.com>
2019-08-19 16:51:31 +0200

    Add Link to PasswordLostLabel component

--- packages/components/src/FormGroup/PasswordLostLabel.tsx
+++ packages/components/src/FormGroup/PasswordLostLabel.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
+import { Link } from '../Link';
+
 interface Props {
   href?: string;
 }
 
 const PasswordLostLabel = ({ href }: Props) => (
-  <a
+  <Link
     href={href || 'https://t3n.de/account/lostpassword'}
     title="Passwort vergessen?"
+    underline="always"
   >
     Passwort vergessen?
-  </a>
+  </Link>
 );
 
 export default PasswordLostLabel;
