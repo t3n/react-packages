30671f294d50465bd64fe48a16c16f1be3f016a5
Johannes Steu <hello@johannessteu.de>
2019-08-23 11:07:45 +0200

    Add missing type definitions

--- packages/theme/src/utils/color.d.ts
+++ packages/theme/src/utils/color.d.ts
@@ -1,2 +1,3 @@
+import { ThemeBackgroundColor } from '../theme/colors/colors';
 export declare const getThemeColor: (c: string) => any;
-export declare const getColorForBackground: (c: any) => any;
+export declare const getColorForBackground: (c: ThemeBackgroundColor) => any;
