f9d7a9e4318740fdc0a35f4c234a83cb9a2cb874
Hannes Heyden <hannesheyden@gmail.com>
2019-08-26 14:45:34 +0200

    prettify Loader.stories.tsx

--- packages/storybook/src/stories/components/content/Loader.stories.tsx
+++ packages/storybook/src/stories/components/content/Loader.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, select} from '@storybook/addon-knobs';
+import { withKnobs, select } from '@storybook/addon-knobs';
 import { Content, Loader } from '@t3n/components';
 
 import StoryContainer from '../../../components/StoryContainer';
@@ -14,7 +14,23 @@ storiesOf('Components|Content/Loader', module)
   ))
   .add(
     'Default',
-    () => (<Loader bg={select('Farbe', {'black': 'shades.black', 'grey42': 'shades.grey42', 'grey155': 'shades.grey155', 'grey232': 'shades.grey232', 'grey244': 'shades.grey244', 'white': 'shades.white', 'red':'brand.red'}, 'shades.grey155')} />),
+    () => (
+      <Loader
+        bg={select(
+          'Farbe',
+          {
+            black: 'shades.black',
+            grey42: 'shades.grey42',
+            grey155: 'shades.grey155',
+            grey232: 'shades.grey232',
+            grey244: 'shades.grey244',
+            white: 'shades.white',
+            red: 'brand.red'
+          },
+          'shades.grey155'
+        )}
+      />
+    ),
     {
       options: {
         showPanel: true
