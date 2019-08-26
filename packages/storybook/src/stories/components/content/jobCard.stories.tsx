d650ea10fe93e88c117bce58d23c288a6d478b3a
Hannes Heyden <hannesheyden@gmail.com>
2019-08-26 14:22:48 +0200

    prettify jobCard.stories.tsx

--- packages/storybook/src/stories/components/content/jobCard.stories.tsx
+++ packages/storybook/src/stories/components/content/jobCard.stories.tsx
@@ -3,8 +3,8 @@ import { storiesOf } from '@storybook/react';
 import { withKnobs, text, boolean, select } from '@storybook/addon-knobs';
 import { Grid, GridItem, Content, JobCard } from '@t3n/components';
 
-import StoryContainer from '../../../components/StoryContainer';
 import { JobCardType } from '@t3n/components/src/JobCard';
+import StoryContainer from '../../../components/StoryContainer';
 
 storiesOf('Components|Content/JobCard', module)
   .addDecorator(withKnobs)
@@ -16,38 +16,42 @@ storiesOf('Components|Content/JobCard', module)
   .add(
     'Default',
     () => {
-      const type : JobCardType = select('Darstellung', {'Job-Card': 'CARD', 'Job-List': 'LIST'}, 'LIST');
+      const type: JobCardType = select(
+        'Darstellung',
+        { 'Job-Card': 'CARD', 'Job-List': 'LIST' },
+        'LIST'
+      );
       const loading = boolean('Ladestatus', false);
-      const p = [type=== 'CARD' ? 1: 0, type=== 'CARD' ? 1 : 0 ,0];
+      const p = [type === 'CARD' ? 1 : 0, type === 'CARD' ? 1 : 0, 0];
 
       const job = {
-        companyName: text('Firmennanme','PB3C GmbH'),
-        imageUrl: text('Firmenlogo','https://storage.googleapis.com/t3n-de/jobslogos/50ee624bec9641caa70b3ff15300b75157ef532b/pb3c.png'),
-        title: text('Jobtitel','(Senior) Performance Marketing Manager (m/w/d)'),
-        url: text('Job-URL','https://t3n.de/jobs/job/pb3c-gmbh/senior-performance-marketing-manager-m-w-d/'),
+        companyName: text('Firmennanme', 'PB3C GmbH'),
+        imageUrl: text(
+          'Firmenlogo',
+          'https://storage.googleapis.com/t3n-de/jobslogos/50ee624bec9641caa70b3ff15300b75157ef532b/pb3c.png'
+        ),
+        title: text(
+          'Jobtitel',
+          '(Senior) Performance Marketing Manager (m/w/d)'
+        ),
+        url: text(
+          'Job-URL',
+          'https://t3n.de/jobs/job/pb3c-gmbh/senior-performance-marketing-manager-m-w-d/'
+        ),
         city: text('Stadt', 'Berlin'),
-        postCode: text('Postleitzahl','10789')
+        postCode: text('Postleitzahl', '10789')
       };
 
       return (
         <Grid justifyContent="center">
-          <GridItem width={[1, 1, type === 'CARD' ? 1/3 : 1 ]} pt={p} pb={p}>
-            <JobCard
-              job={job}
-              loading={loading} type={type}
-            />
+          <GridItem width={[1, 1, type === 'CARD' ? 1 / 3 : 1]} pt={p} pb={p}>
+            <JobCard job={job} loading={loading} type={type} />
           </GridItem>
-          <GridItem width={[1, 1, type === 'CARD' ? 1/3 : 1 ]} pt={p} pb={p}>
-            <JobCard
-              job={job}
-              loading={loading} type={type}
-            />
+          <GridItem width={[1, 1, type === 'CARD' ? 1 / 3 : 1]} pt={p} pb={p}>
+            <JobCard job={job} loading={loading} type={type} />
           </GridItem>
-          <GridItem width={[1, 1, type === 'CARD' ? 1/3 : 1 ]} pt={p} pb={p}>
-            <JobCard
-              job={job}
-              loading={loading} type={type}
-            />
+          <GridItem width={[1, 1, type === 'CARD' ? 1 / 3 : 1]} pt={p} pb={p}>
+            <JobCard job={job} loading={loading} type={type} />
           </GridItem>
         </Grid>
       );
