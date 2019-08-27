1ed9c8245aedba47f02e3e6b84308ec9d5f51b8a
Philip Plagge <sayhi@itscursedphil.com>
2019-08-27 14:28:42 +0200

    Extract Link styles

--- packages/components/src/PageLayout/PageLayout.d.ts
+++ packages/components/src/PageLayout/PageLayout.d.ts
@@ -2,6 +2,7 @@ import React from 'react';
 import { PageHeaderProps } from '../PageHeader/PageHeader';
 interface PageLayoutProps extends PageHeaderProps {
     showHeader: boolean;
+    noContentPadding?: boolean;
     headerContent?: JSX.Element;
 }
 export declare const PageLayout: React.FC<PageLayoutProps>;
