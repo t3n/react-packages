2d2d421d2b7f2f638e21c1e6a47e7b3110bab3a4
Hannes Heyden <hannesheyden@gmail.com>
2019-08-28 16:26:28 +0200

    add fixed margin bottom and default content to titleCard

--- packages/storybook/src/stories/components/content/titleCard.stories.tsx
+++ packages/storybook/src/stories/components/content/titleCard.stories.tsx
@@ -24,7 +24,10 @@ storiesOf('Components|Content/TitleCard', module)
             'h3'
           )}
         >
-          Hallo
+          {text(
+            'Content',
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam'
+          )}
         </TitleCard>
       );
     },
