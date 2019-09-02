887709ec1ac48eb083bb38522d4fa179538ad069
Hannes Heyden <hannesheyden@gmail.com>
2019-09-02 14:17:48 +0200

    Add Footer component and update pageLayout

--- packages/components/src/PageLayout/PageLayout.d.ts
+++ packages/components/src/PageLayout/PageLayout.d.ts
@@ -1,9 +1,10 @@
 import React from 'react';
 import { PageHeaderProps } from '../PageHeader/PageHeader';
 interface PageLayoutProps extends PageHeaderProps {
-    showHeader: boolean;
+    showHeader?: boolean;
     noContentPadding?: boolean;
     headerContent?: JSX.Element;
+    footerContent?: JSX.Element;
 }
 export declare const PageLayout: React.FC<PageLayoutProps>;
 export {};
