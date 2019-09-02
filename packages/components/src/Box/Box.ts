0ccaf81c9ee47847bbc2cd925132a09a098dcc3b
Hannes Heyden <hannesheyden@gmail.com>
2019-09-02 11:05:26 +0200

    add box-component and adjust text-compnent

---
+++ packages/components/src/Box/Box.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import {
+  space,
+  color,
+  layout,
+  flexbox,
+  position,
+  SpaceProps,
+  ColorProps,
+  LayoutProps,
+  FlexboxProps,
+  PositionProps
+} from 'styled-system';
+
+export interface BoxProps
+  extends SpaceProps,
+    ColorProps,
+    LayoutProps,
+    FlexboxProps,
+    PositionProps {}
+
+export const Box = styled.div<BoxProps>`
+  ${space}
+  ${color}
+  ${layout}
+  ${flexbox}
+  ${position}
+`;
