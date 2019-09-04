f783ecfd5a2072b2f027423105e6e24531f0281b
Philip Plagge <sayhi@itscursedphil.com>
2019-09-04 17:58:07 +0200

    Update components to use icons from icon package

--- packages/components/src/Logo/Logo.tsx
+++ packages/components/src/Logo/Logo.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import DefaultLogo from './DefaultLogo.svg';
+import styled from 'styled-components';
+import { T3nDefaultLogo } from '@t3n/icons';
 
 export type LogoVariant = 'default';
 
-interface LogoProps {
-  height?: string;
-  width?: string;
+interface LogoProps extends React.SVGAttributes<SVGElement> {
   variant?: LogoVariant;
 }
 
-export const Logo: React.FC<LogoProps> = ({ variant, ...rest }) => {
+export const Logo = styled(({ variant, ...props }) => {
   // todo check for other variants
   if (variant && variant === 'default') {
-    return <DefaultLogo {...rest} />;
+    return <T3nDefaultLogo width="3rem" height="1rem" {...props} />;
   }
 
-  return <DefaultLogo {...rest} />;
-};
+  return <T3nDefaultLogo width="3rem" height="1rem" {...props} />;
+})<LogoProps>``;
