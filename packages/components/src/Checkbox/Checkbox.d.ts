7644c18a75c71c3c3ef80fa0ea577f6db81e7450
Hannes Heyden <hannesheyden@gmail.com>
2019-09-12 16:32:42 +0200

    export checkboxprops and add name

--- packages/components/src/Checkbox/Checkbox.d.ts
+++ packages/components/src/Checkbox/Checkbox.d.ts
@@ -1,12 +1,15 @@
 import React from 'react';
 import { ColorProps } from 'styled-system';
 import { ThemeFeedbackColor } from '@t3n/theme/src/theme/colors/colors';
-interface CheckboxProps extends ColorProps {
-    checked: boolean;
+interface StyledCheckboxProps extends ColorProps {
     disabled?: boolean;
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
     label?: string;
     feedbackColor?: ThemeFeedbackColor;
 }
-export declare const Checkbox: ({ checked, onChange, label, disabled, feedbackColor }: CheckboxProps) => JSX.Element;
+export interface CheckboxProps extends StyledCheckboxProps {
+    name: string;
+    checked: boolean;
+}
+export declare const Checkbox: ({ checked, onChange, label, disabled, feedbackColor, name }: CheckboxProps) => JSX.Element;
 export {};
