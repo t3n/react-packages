7f530a8158eebee1a5ba4c7a6732bc6ea9778819
Philip Plagge <sayhi@itscursedphil.com>
2019-09-16 16:21:00 +0200

    Fix storybook package and add storybook-docs

--- packages/components/src/Placeholder/Placeholder.d.ts
+++ packages/components/src/Placeholder/Placeholder.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="react" />
+/// <reference types="@emotion/core" />
 import { SpaceProps, LayoutProps, BorderRadiusProps } from 'styled-system';
 export declare const Placeholder: import("styled-components").StyledComponent<"div", any, SpaceProps<import("react").ReactText> | LayoutProps | BorderRadiusProps<import("react").ReactText>, never>;
--- packages/configs/webpack/storybook.js
+++ packages/configs/webpack/storybook.js
@@ -8,29 +8,12 @@ const WebpackNotifierPlugin = require('webpack-notifier');
 module.exports = ({ config }) => {
   config.devtool = 'source-map';
 
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    use: [
-      {
-        loader: require.resolve('awesome-typescript-loader'),
-        options: {
-          forceIsolatedModules: true,
-          useCache: true,
-          useBabel: true,
-          babelCore: '@babel/core',
-          reportFiles: ['src/**/*.{ts,tsx}']
-        }
-      }
-    ]
-  });
-
   config.module.rules[0] = {
     ...config.module.rules[0],
     enforce: 'pre',
     use: [...config.module.rules[0].use, { loader: 'source-map-loader' }]
   };
 
-  config.resolve.extensions.push('.ts', '.tsx');
   config.plugins.push(new WebpackNotifierPlugin({ title: '@t3n/storybook' }));
 
   return config;
