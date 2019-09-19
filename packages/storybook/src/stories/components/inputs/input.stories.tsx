6bc63e3571dd7c647f55c90b9915f0d6c02e216f
Johannes Steu <hello@johannessteu.de>
2019-09-19 19:36:34 +0200

    Fix FormGroup label margin to align with error messages

--- packages/storybook/src/stories/components/inputs/input.stories.tsx
+++ packages/storybook/src/stories/components/inputs/input.stories.tsx
@@ -1,27 +1,51 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { Input } from '@t3n/components';
+import { Input, FormGroup } from '@t3n/components';
 
 import StoryContainer from '../../../components/StoryContainer';
 
-storiesOf('Components|Inputs/Input', module).add(
-  'Simple Input',
-  () => (
-    <StoryContainer>
-      <Input
-        type="text"
-        placeholder="Firstname Lastname"
-        fixedPlaceholder="Name:"
-        onChange={e => {
-          console.log(e.currentTarget.value);
-        }}
-      />
-    </StoryContainer>
-  ),
-  {
-    options: {
-      showPanel: true
+storiesOf('Components|Inputs/Input', module)
+  .add(
+    'Simple Input',
+    () => (
+      <StoryContainer>
+        <Input
+          type="text"
+          placeholder="Firstname Lastname"
+          fixedPlaceholder="Name:"
+          onChange={e => {
+            console.log(e.currentTarget.value);
+          }}
+        />
+      </StoryContainer>
+    ),
+    {
+      options: {
+        showPanel: true
+      }
     }
-  }
-);
+  )
+  .add('Form Group', () => (
+    <StoryContainer>
+      <FormGroup label="Vorname">
+        <Input
+          type="text"
+          placeholder="Johannes"
+          onChange={e => {
+            console.log(e.currentTarget.value);
+          }}
+        />
+      </FormGroup>
+      <FormGroup label="Vorname" errorMessage="Dies ist eine Error-Message">
+        <Input
+          type="text"
+          placeholder="Johannes"
+          error
+          onChange={e => {
+            console.log(e.currentTarget.value);
+          }}
+        />
+      </FormGroup>
+    </StoryContainer>
+  ));
