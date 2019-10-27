682b47f82d83b0ca75ede5e03b45e49eab843be9
Johannes Steu <hello@johannessteu.de>
2019-10-27 15:44:04 +0100

    Handle reset in FormField

--- packages/storybook/src/components/FormField.tsx
+++ packages/storybook/src/components/FormField.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useField } from 'formik';
+import { useField, useFormikContext } from 'formik';
 import { FormGroup, Input, PasswordLostLabel } from '@t3n/components';
 
 import { InputTypes } from '@t3n/components/src/Input/Input';
@@ -14,8 +14,13 @@ const FormInput = ({ name, label, type = 'text' }: FormInputProps) => {
   const [{ value, name: fieldName, ...rest }, { touched, error }] = useField(
     name
   );
+  const { setFieldValue } = useFormikContext<any>();
 
   const hasErrors = !!(touched && error);
+
+  const handleReset = () => {
+    setFieldValue(name, '');
+  };
 
   return (
     <>
@@ -24,7 +29,13 @@ const FormInput = ({ name, label, type = 'text' }: FormInputProps) => {
         errorMessage={touched && error ? error : ''}
         labelEndContent={name === 'password' && <PasswordLostLabel />}
       >
-        <Input name={fieldName} error={hasErrors} type={type} {...rest} />
+        <Input
+          name={fieldName}
+          error={hasErrors}
+          type={type}
+          onReset={handleReset}
+          {...rest}
+        />
       </FormGroup>
     </>
   );
