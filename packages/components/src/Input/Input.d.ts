196a982b6ecf096f279efa366f295c67a2cad32f
Johannes Steu <hello@johannessteu.de>
2019-10-27 10:28:16 +0100

    Reafactor input component

--- packages/components/src/Input/Input.d.ts
+++ packages/components/src/Input/Input.d.ts
@@ -1,18 +1,19 @@
 import React from 'react';
 import { WidthProps } from 'styled-system';
 export declare type InputTypes = 'text' | 'email' | 'password';
-export interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'width'>, WidthProps {
+export interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'width' | 'value'>, WidthProps {
     type?: InputTypes;
-    value?: string;
+    onReset?: () => void;
+    isFocused: boolean;
     defaultValue?: string;
     error?: boolean;
-    fixedPlaceholder?: string;
     className?: string;
 }
 export declare const Input: {
-    ({ type, value, defaultValue, disabled, error, fixedPlaceholder, width, className, onChange, onFocus, onBlur, ...props }: InputProps): JSX.Element;
+    ({ disabled, type, error, width, className, onFocus, onBlur, onChange, onReset, isFocused, defaultValue, ...props }: InputProps): JSX.Element;
     defaultProps: {
         type: string;
+        isFocused: boolean;
         width: string;
     };
 };
--- packages/storybook/src/stories/components/inputs/input.stories.tsx
+++ packages/storybook/src/stories/components/inputs/input.stories.tsx
@@ -1,46 +1,63 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
-import { Input, FormGroup } from '@t3n/components';
-
-import { storyContainerDecorator } from '../../../utils/decorators';
+import { Input, Text, FormGroup } from '@t3n/components';
+import { storyContainerContentDecorator } from '../../../utils/decorators';
 
 export default {
   title: 'Components|Inputs/Input',
   component: Input,
-  decorators: [storyContainerDecorator]
+  decorators: [storyContainerContentDecorator]
 };
 
-export const simple = () => (
-  <Input
-    type="text"
-    placeholder="Firstname Lastname"
-    fixedPlaceholder="Name:"
-    onChange={e => {
-      console.log(e.currentTarget.value);
-    }}
-  />
+export const defaultStory = () => (
+  <>
+    <Text>Ohne Value und Placeholder</Text>
+    <Input width={0.5} />
+    <Text>Mit Placeholder</Text>
+    <Input width={0.5} placeholder="Placeholder" />
+    <Text>Mit Value</Text>
+    <Input width={0.5} defaultValue="Max Mustermann" />
+  </>
 );
 
-export const formGroup = () => (
+defaultStory.story = {
+  name: 'Default'
+};
+
+export const disabled = () => (
   <>
-    <FormGroup label="Vorname">
-      <Input
-        type="text"
-        placeholder="Johannes"
-        onChange={e => {
-          console.log(e.currentTarget.value);
-        }}
-      />
-    </FormGroup>
-    <FormGroup label="Vorname" errorMessage="Dies ist eine Error-Message">
-      <Input
-        type="text"
-        placeholder="Johannes"
-        error
-        onChange={e => {
-          console.log(e.currentTarget.value);
-        }}
-      />
-    </FormGroup>
+    <Text>Ohne Value und Placeholder</Text>
+    <Input disabled width={0.5} />
+    <Text>Mit Placeholder</Text>
+    <Input disabled width={0.5} placeholder="Placeholder" />
+    <Text>Mit Value</Text>
+    <Input disabled width={0.5} defaultValue="Max Mustermann" />
+  </>
+);
+
+export const focused = () => <Input isFocused width={0.5} />;
+
+focused.story = {
+  name: 'Focus'
+};
+
+export const types = () => (
+  <>
+    <Text>Text:</Text>
+    <Input type="text" width={0.5} defaultValue="Max Mustermann" />
+    <Text>E-Mail:</Text>
+    <Input type="email" width={0.5} defaultValue="support@t3n.de" />
+    <Text>Password:</Text>
+    <Input type="password" width={0.5} defaultValue="support@t3n.de" />
+  </>
+);
+
+types.story = {
+  name: 'Types'
+};
+
+export const errorStates = () => (
+  <>
+    <Input error width={0.5} defaultValue="Invalid" />
   </>
 );
