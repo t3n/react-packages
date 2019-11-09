1e794f53c38292b3bf03889d956de774c6c81e35
Johannes Steu <hello@johannessteu.de>
2019-11-09 19:55:03 +0100

    Remove old info stories

--- packages/storybook/.storybook/config.js
+++ packages/storybook/.storybook/config.js
@@ -55,8 +55,6 @@ addDecorator(story => (
 const loadStories = () => [
   // Storybook
   require('../src/stories/storybook/introduction.story.mdx'),
-  require('../src/stories/storybook/usage.story.mdx'),
-  require('../src/stories/storybook/editor.story.mdx'),
 
   // Design-System
   require('../src/stories/designSystem/breakpoints/breakpoints.stories'),
