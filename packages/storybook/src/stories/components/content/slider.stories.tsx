8a352bf6cef8dd9ac4a287af8ab901ed5260c338
Markus Günther <info@unikka.de>
2019-11-22 13:32:19 +0100

    FEATURE: Define new stories for the slider storybook

--- packages/storybook/src/stories/components/content/slider.stories.tsx
+++ packages/storybook/src/stories/components/content/slider.stories.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import {
-  Slider
-} from '@t3n/components';
+import React, { useState } from 'react';
+import { Slider, Heading, Grid, GridItem } from '@t3n/components';
+import { theme } from '@t3n/theme';
 
 export default {
   title: 'Components|Content/Slider',
@@ -11,7 +10,7 @@ export default {
 export const defaultStory = () => {
   return (
     <div>
-      <Slider name="slider" minValue={3} maxValue={10} initialValue={5} />
+      <Slider name="slider" minValue={1} maxValue={5} initialValue={2} />
     </div>
   );
 };
@@ -36,29 +35,81 @@ export const customMinMaxWithStepsStory = () => {
 };
 
 customMinMaxWithStepsStory.story = {
-  name: 'Custom minimum and maximum values in defined steps'
+  name: 'Minimum und maximum Werte in definierten Schritten'
 };
 
-export const withTracksStory = () => {
+export const customHighlightColorStory = () => {
   return (
-    <div>
-      <Slider
-        name="slider"
-        initialValue={5}
-        minValue={3}
-        maxValue={20}
-        tracks={
-          [
+    <Grid>
+      <GridItem>
+        <Heading as="h3" mt={0}>
+          Default Pointer (rot)
+        </Heading>
+        <Slider name="slider" minValue={1} maxValue={5} initialValue={2} />
+      </GridItem>
+      <GridItem>
+        <Heading as="h3" mt={0}>
+          Schwarzer Pointer
+        </Heading>
+        <Slider
+          name="slider"
+          minValue={1}
+          maxValue={5}
+          initialValue={2}
+          highlightColor={theme.colors.brand.black}
+        />
+      </GridItem>
+      <GridItem>
+        <Heading as="h3" mt={0}>
+          Grüner Pointer
+        </Heading>
+        <Slider
+          name="slider"
+          minValue={1}
+          maxValue={5}
+          initialValue={2}
+          highlightColor={theme.colors.feedback.success}
+        />
+      </GridItem>
+    </Grid>
+  );
+};
+
+customHighlightColorStory.story = {
+  name: 'Angepasste Farben der Pointer'
+};
+
+export const withOnChangeStory = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [selectedValue, setSelectedValue] = useState(5);
+  const handleOnChange = (value: number) => {
+    setSelectedValue(value);
+  };
+
+  return (
+    <Grid>
+      <GridItem>
+        <Slider
+          name="slider"
+          initialValue={5}
+          minValue={3}
+          maxValue={20}
+          tracks={[
             { label: '3 €', value: 3, showLabel: true },
             { label: '5 €', value: 5, showLabel: true },
             { label: '10 €', value: 10, showLabel: true },
             { label: '20 €', value: 20, showLabel: true }
-          ]
-        } />
-    </div>
+          ]}
+          onChange={handleOnChange}
+        />
+        <p>
+          <strong>Your selected value:</strong> {selectedValue}
+        </p>
+      </GridItem>
+    </Grid>
   );
 };
 
-withTracksStory.story = {
-  name: 'Slider with tracks as steps'
+withOnChangeStory.story = {
+  name: 'Slider mit Tracks als Schritte und eine Funktion zur Ausgabe'
 };
