2014a9a06b6dd25333f30283c45cfe73d519cc7a
Johannes Steu <hello@johannessteu.de>
2019-12-09 15:01:43 +0100

    Add missing link definitions

--- packages/components/src/Link/Link.d.ts
+++ packages/components/src/Link/Link.d.ts
@@ -10,6 +10,6 @@ export interface LinkProps extends TextColorProps, SpaceProps {
     variant?: LinkVariantType;
 }
 export declare type LinkState = 'default' | 'hover' | 'focus' | 'visited';
-export declare const createLinkStyle: (linkStyleConfig: LinkStyle) => import("styled-components").FlattenInterpolation<import("styled-components").ThemedStyledProps<Pick<LinkProps, "p" | "small" | "color" | "m" | "margin" | "mt" | "marginTop" | "mb" | "marginBottom" | "ml" | "marginLeft" | "mr" | "marginRight" | "my" | "mx" | "disabled" | "marginX" | "marginY" | "padding" | "pt" | "paddingTop" | "pr" | "paddingRight" | "pb" | "paddingBottom" | "pl" | "paddingLeft" | "px" | "paddingX" | "py" | "paddingY" | "variant">, any>>;
+export declare const createLinkStyle: (linkStyleConfig: LinkStyle) => import("styled-components").FlattenInterpolation<import("styled-components").ThemedStyledProps<Pick<LinkProps, "p" | "small" | "color" | "disabled" | "variant" | "m" | "margin" | "mt" | "marginTop" | "mr" | "marginRight" | "mb" | "marginBottom" | "ml" | "marginLeft" | "mx" | "marginX" | "my" | "marginY" | "padding" | "pt" | "paddingTop" | "pr" | "paddingRight" | "pb" | "paddingBottom" | "pl" | "paddingLeft" | "px" | "paddingX" | "py" | "paddingY">, any>>;
 export declare const linkStyle: import("styled-components").FlattenInterpolation<import("styled-components").ThemedStyledProps<LinkProps, any>>;
 export declare const Link: import("styled-components").StyledComponent<"a", any, LinkProps, never>;
