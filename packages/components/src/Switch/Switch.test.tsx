71d608405ae6708c4f1848815e6e49afb3d72be3
Hannes Heyden <hannesheyden@gmail.com>
2020-01-06 14:17:30 +0100

    add tests for switch component

---
+++ packages/components/src/Switch/Switch.test.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { renderWithTheme } from '../helper/renderWithTheme';
+import { Switch } from './Switch';
+
+test('readonly is set as HTML Attribute', () => {
+  const { getByRole } = renderWithTheme(
+    <Switch
+      disabled={false}
+      checked={false}
+      value="true"
+      name="Read-Only"
+      readOnly
+      label="Switch read only"
+      variant="light"
+    />,
+    {}
+  );
+
+  expect(getByRole('checkbox').hasAttribute('readonly')).toBeTruthy();
+});
+
+test('Switch variants matches snapshot', () => {
+  const { container } = renderWithTheme(
+    <>
+      <Switch
+        disabled={false}
+        checked={false}
+        value="true"
+        name="unchecked"
+        readOnly
+        label="Switch unchecked"
+        variant="light"
+      />
+      <Switch
+        disabled={false}
+        checked
+        value="true"
+        name="checked"
+        readOnly
+        label="Switch checked"
+        variant="light"
+      />
+      <Switch
+        disabled
+        checked
+        value="true"
+        name="disabled"
+        readOnly
+        label="Switch disabled checked"
+        variant="light"
+      />
+      <Switch
+        disabled
+        checked={false}
+        value="true"
+        name="disabled"
+        readOnly
+        label="Switch disabled unchecked"
+        variant="light"
+      />
+      <Switch
+        disabled={false}
+        checked={false}
+        value="true"
+        name="unchecked"
+        readOnly
+        label="Switch unchecked"
+        variant="dark"
+      />
+      <Switch
+        disabled={false}
+        checked
+        value="true"
+        name="checked"
+        readOnly
+        label="Switch checked"
+        variant="dark"
+      />
+      <Switch
+        disabled
+        checked
+        value="true"
+        name="disabled"
+        readOnly
+        label="Switch disabled checked"
+        variant="dark"
+      />
+      <Switch
+        disabled
+        checked={false}
+        value="true"
+        name="disabled"
+        readOnly
+        label="Switch disabled unchecked"
+        variant="dark"
+      />
+    </>,
+    {}
+  );
+  expect(container).toMatchSnapshot();
+});
