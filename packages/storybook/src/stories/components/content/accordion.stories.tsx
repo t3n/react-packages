d9beebcea90e6fff8186018a4264c17bb642ee3b
Birte Rohden <birte.rohden@gmail.com>
2020-03-24 10:08:58 +0100

    Refine Accordion component

--- packages/storybook/src/stories/components/content/accordion.stories.tsx
+++ packages/storybook/src/stories/components/content/accordion.stories.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Accordion,
   Section,
+  Button,
   Image,
   Text,
   Grid,
@@ -53,11 +54,16 @@ isOpenStory.story = {
   name: 'Initial geöffnet'
 };
 
-export const multipleStory = () => {
+const MultiAccordions = () => {
+  const [accState, setAccState] = useState(true);
+
   return (
     <Section variant="secondary">
+      <Button onClick={() => setAccState(!accState)} mb={3}>
+        {accState ? 'Alle schließen' : 'Alle öffnen'}
+      </Button>
       <Accordion
-        initialOpen
+        initialOpen={accState}
         title="Wie bekomme ich einen grünen Haken und damit ein verifiziertes Profil?"
         mb={1}
       >
@@ -70,6 +76,7 @@ export const multipleStory = () => {
         </Text>
       </Accordion>
       <Accordion
+        initialOpen={accState}
         title="Warum sollte ich mich beim Pioneers Network anmelden?"
         mb={1}
       >
@@ -82,24 +89,30 @@ export const multipleStory = () => {
           immer wieder reinzuschauen und von Anfang an dabei zu sein!
         </Text>
       </Accordion>
-      <Accordion title="Kann ich mich mit anderen Pionieren vernetzen?" mb={1}>
+      <Accordion
+        initialOpen={accState}
+        title="Kann ich mich mit anderen Pionieren vernetzen?"
+        mb={1}
+      >
         <Text mt={0}>
           In der ersten Version des Pioneers Network gibt es diese Funktion
           nicht. Für die Zukunft ist das als Feature vorgesehen.
         </Text>
       </Accordion>
       <Accordion
+        initialOpen={accState}
         title="Das hier ist eine ganz lange Headline um zu testen wie ein Umbruch aussehen würde."
         mb={1}
       >
         <Grid>
-          <GridItem width={[1, 1, 1 / 3]}>
+          <GridItem width={[1, 1, 1 / 4]}>
             <Image
+              width="200px"
               alt="Bild"
               src="https://images.unsplash.com/photo-1559116674-71fae8b32c95?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=663&q=80"
             />
           </GridItem>
-          <GridItem width={[1, 1, 2 / 3]}>
+          <GridItem width={[1, 1, 3 / 4]}>
             <Text mt={0}>
               Im Pioneers Network kannst du andere digitale Pioniere entdecken
               und Einblicke in ihre Arbeit bekommen. Sie geben dir Tipps aus
@@ -115,6 +128,8 @@ export const multipleStory = () => {
     </Section>
   );
 };
+
+export const multipleStory = () => <MultiAccordions />;
 
 multipleStory.story = {
   name: 'Mehrere Akkordeons'
