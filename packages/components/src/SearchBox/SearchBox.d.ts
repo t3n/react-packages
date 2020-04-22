b9a63125739843eb44055457c52bf82c3a413c65
Philip Plagge <plaggep@gmail.com>
2020-04-22 12:20:32 +0200

    Add new SearchBox features

--- packages/components/src/SearchBox/SearchBox.d.ts
+++ packages/components/src/SearchBox/SearchBox.d.ts
@@ -1,4 +1,4 @@
-/// <reference types="react" />
+import React from 'react';
 import { RenderSuggestion, SuggestionsFetchRequested, OnSuggestionsClearRequested, GetSuggestionValue, OnSuggestionSelected } from 'react-autosuggest';
 import { WidthProps } from 'styled-system';
 export interface GroupedSuggestions<S> {
@@ -10,22 +10,26 @@ export interface SearchBoxProps<S> extends WidthProps {
     variant: SearchBoxVariantType;
     placeholder: string;
     isLoading: boolean;
+    defaultValue?: string;
     multiSection?: boolean;
     suggestions: GroupedSuggestions<S>[] | S[] | null;
     getSuggestionValue: GetSuggestionValue<S>;
     handleSuggestionFetchRequested: SuggestionsFetchRequested;
     handleSuggestionClearRequested: OnSuggestionsClearRequested;
     renderSuggestion: RenderSuggestion<S>;
+    renderSuggestionsEmpty?: React.ReactNode;
     onSelect: OnSuggestionSelected<S>;
+    clearOnSelect?: boolean;
     onSearchTermChange?: (term: string) => void;
 }
-declare function SearchBox<S>({ variant: variantProp, width, placeholder, multiSection, isLoading, renderSuggestion, suggestions, onSelect, onSearchTermChange, getSuggestionValue, handleSuggestionFetchRequested, handleSuggestionClearRequested, }: SearchBoxProps<S>): JSX.Element;
+declare function SearchBox<S>({ variant: variantProp, width, placeholder, defaultValue, multiSection, isLoading, renderSuggestion, renderSuggestionsEmpty: SuggestionsEmpty, suggestions, onSelect, clearOnSelect, onSearchTermChange, getSuggestionValue, handleSuggestionFetchRequested, handleSuggestionClearRequested, }: SearchBoxProps<S>): JSX.Element;
 declare namespace SearchBox {
     var defaultProps: {
         isLoading: boolean;
         suggestions: null;
         variant: string;
         placeholder: string;
+        clearOnSelect: boolean;
     };
 }
 export { SearchBox };
