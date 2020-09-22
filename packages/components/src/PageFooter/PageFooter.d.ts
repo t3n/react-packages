d708c032b60e9d87a98b4a19422d4b4105ee3b2c
Philip Plagge <plaggep@gmail.com>
2020-09-22 12:22:02 +0200

    Add optional privacy settings link to page footer component

--- packages/components/src/PageFooter/PageFooter.d.ts
+++ packages/components/src/PageFooter/PageFooter.d.ts
@@ -1,6 +1,7 @@
 import React from 'react';
 export interface PageFooterProps {
     contactLink: string;
+    showPrivacySettingsLink?: boolean;
 }
 export declare const FooterLink: import("styled-components").StyledComponent<"a", any, import("../Link").LinkProps & {
     small: boolean;
