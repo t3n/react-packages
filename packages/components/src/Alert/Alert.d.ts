f23dcf121834d9caae30c96effc09d025d5a50a8
Hannes Heyden <hannesheyden@gmail.com>
2020-11-25 11:15:12 +0100

    update alert defintion

--- packages/components/src/Alert/Alert.d.ts
+++ packages/components/src/Alert/Alert.d.ts
@@ -4,6 +4,5 @@ export declare type AlertStatus = 'success' | 'notice' | 'warning' | 'error';
 export interface AlertProps extends SpaceProps {
     status: AlertStatus;
 }
-declare const AlertText: React.FC;
-declare const Alert: React.FC<AlertProps>;
-export { Alert, AlertText };
+export declare const AlertText: React.FC;
+export declare const Alert: React.FC<AlertProps>;
