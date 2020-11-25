bdc3a8fc34483a91af9a5c6086ef4da3b4af3e02
Philip Plagge <plaggep@gmail.com>
2020-11-25 11:13:57 +0100

    Run lint on build via webpack

--- packages/configs/webpack/index.js
+++ packages/configs/webpack/index.js
@@ -3,6 +3,7 @@
 const { resolve } = require('path');
 const WebpackNotifierPlugin = require('webpack-notifier');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 const babelConfig = require('../babel');
 
@@ -106,6 +107,9 @@ module.exports = ({ title = '', dirname = '' }) => {
         analyzerMode: 'static',
         openAnalyzer: false,
         reportFilename: './report/report.html',
+      }),
+      new ESLintPlugin({
+        extensions: ['ts', 'tsx', 'js', 'jsx'],
       }),
       ...config.plugins,
     ];
