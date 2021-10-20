d5e7ca4dffe98fffc9c03d12327606f71e95e498
Birte Rohden <birte.rohden@gmail.com>
2021-10-20 14:08:21 +0200

    add LegacyBookmark button component to use it in LegacyNewsCard

---
+++ packages/components/src/LegacyNewsCard/LegacyBookmark.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+import { border, color, space, typography } from 'styled-system';
+
+import BookmarkBorder from '@t3n/icons/src/components/material/action/BookmarkBorder';
+
+import { Box } from '../Box';
+
+const TooltipContainer = styled(Box)`
+  cursor: pointer;
+  z-index: 50;
+
+  &:hover {
+    > div > svg {
+      fill: #000;
+    }
+  }
+
+  &:hover {
+    > span {
+      opacity: 1;
+    }
+  }
+`;
+
+const Tooltip = styled.span`
+  position: absolute;
+  opacity: 0;
+  transition: all 0.1s ease-out;
+  pointer-events: none;
+  display: inline-block;
+  left: 50%;
+  transform: translateX(-50%);
+  white-space: nowrap;
+  top: 100%;
+
+  ${({ theme }) => space({ theme, px: '4px', py: '6px' })};
+  ${({ theme }) =>
+    border({
+      theme,
+      borderRadius: '4px',
+    })};
+  ${({ theme }) =>
+    color({ theme, bg: 'background.highlight', color: 'text.inverse' })}
+  ${({ theme }) => typography({ theme, fontSize: '12px' })}
+`;
+
+const LegacyBookmark: React.FC<{ onClick: () => void }> = ({ onClick }) => {
+  return (
+    <TooltipContainer display="inline-block" position="relative">
+      <Box id="bookmark" onClick={onClick}>
+        <BookmarkBorder fill="#8F8F8F" width="20" height="20" />
+      </Box>
+      <Tooltip>Artikel merken</Tooltip>
+    </TooltipContainer>
+  );
+};
+
+export default LegacyBookmark;
