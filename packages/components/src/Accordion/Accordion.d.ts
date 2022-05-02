44a9977c683cec73f25cad6d82f7e51a6b49d3e8
Birte Rohden <birte.rohden@gmail.com>
2022-05-02 13:11:51 +0200

    forgot to run build
    coontributes to #PURPLE-143

--- packages/components/src/Accordion/Accordion.d.ts
+++ packages/components/src/Accordion/Accordion.d.ts
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MarginProps } from 'styled-system';
 export interface AccordionProps extends MarginProps {
-    title: string;
+    title: string | JSX.Element;
     initialOpen?: boolean;
 }
 declare const Accordion: React.FC<AccordionProps>;
